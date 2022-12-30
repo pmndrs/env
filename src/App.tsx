59feb872bee2d8b40eaf9b943e1e69d47342360f
Umar Ahmed <umar.ahmed1998@gmail.com>
2022-12-29 20:19:51 -0500

    upgrade libs

--- src/App.tsx
+++ src/App.tsx
@@ -350,28 +350,6 @@ function Porsche(props: any) {
       (node: any) =>
         node.isMesh && (node.receiveShadow = node.castShadow = true)
     );
-    applyProps(materials.rubber, {
-      color: "#222",
-      roughness: 0.6,
-      roughnessMap: null,
-      normalScale: [4, 4],
-    });
-    applyProps(materials.window, {
-      color: "black",
-      roughness: 0,
-      clearcoat: 0.1,
-    });
-    applyProps(materials.coat, {
-      envMapIntensity: 4,
-      roughness: 0.5,
-      metalness: 1,
-    });
-    applyProps(materials.paint, {
-      roughness: 0.5,
-      metalness: 0.8,
-      color: "#555",
-      envMapIntensity: 2,
-    });
   }, [nodes, materials]);
   return <primitive object={scene} {...props} />;
 }

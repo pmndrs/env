80f23aa24293e7fcfb677ddc782e6dca2df1eb8a
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-07-31 21:47:38 -0400

    remove pivot controls

--- src/components/Model/index.tsx
+++ src/components/Model/index.tsx
@@ -1,5 +1,5 @@
-import { PivotControls, Resize, useCursor, useGLTF } from "@react-three/drei";
-import { useMemo, useState } from "react";
+import { Resize, useGLTF } from "@react-three/drei";
+import { useMemo } from "react";
 import * as THREE from "three";
 import { useStore } from "../../hooks/useStore";
 
@@ -33,40 +33,9 @@ export function Model({ debugMaterial, ...props }: any) {
     });
   }, [nodes, materials, debugMaterial]);
 
-  const [isSelected, setIsSelected] = useState(false);
-  const [hovered, setHovered] = useState(false);
-  useCursor(hovered, "pointer");
-
   return (
-    <PivotControls
-      visible={isSelected}
-      disableSliders={!isSelected}
-      disableAxes={!isSelected}
-      disableRotations={!isSelected}
-      depthTest={false}
-    >
-      <group
-        onClick={(e) => {
-          if (!isSelected) {
-            e.stopPropagation();
-            setIsSelected(!isSelected);
-          }
-        }}
-        onPointerMissed={(e) => {
-          if (e.type === "click") {
-            setIsSelected(false);
-          }
-        }}
-        onPointerOver={(e) => {
-          e.stopPropagation();
-          setHovered(true);
-        }}
-        onPointerOut={(e) => setHovered(false)}
-      >
-        <Resize scale={2}>
-          <primitive object={scene} {...props} />
-        </Resize>
-      </group>
-    </PivotControls>
+    <Resize scale={2}>
+      <primitive object={scene} {...props} />
+    </Resize>
   );
 }

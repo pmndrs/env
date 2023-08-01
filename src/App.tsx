24fbc6e0f8b002a5e9696c323db30673884d2f59
Umar Ahmed <umar.ahmed1998@gmail.com>
2023-07-31 21:58:46 -0400

    styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,18 @@ export default function App() {
 
       <AppToolbar />
 
-      <PanelGroup direction="horizontal" className="flex-1 p-4 pt-1">
+      <PanelGroup direction="horizontal" className="flex-1 p-2 pt-1">
         {/* Left */}
         <Panel
           collapsible
-          defaultSize={15}
+          defaultSize={25}
           className="shadow-inner shadow-white/5 ring-offset-white/10 ring-offset-1 ring-1 ring-black/20 bg-neutral-900 rounded-lg"
         >
           <Outliner />
         </Panel>
 
         <PanelResizeHandle className="w-2 grid place-items-center transition-all hover:bg-white/40 rounded-sm mx-1">
-          <Bars2Icon className="rotate-90 -m-1" />
+          <Bars2Icon className="rotate-90 -m-1 text-white/50" />
         </PanelResizeHandle>
 
         {/* Middle */}
@@ -46,13 +46,13 @@ export default function App() {
         </Panel>
 
         <PanelResizeHandle className="w-2 grid place-items-center transition-all hover:bg-white/40 rounded-sm mx-1">
-          <Bars2Icon className="rotate-90 -m-1" />
+          <Bars2Icon className="rotate-90 -m-1 text-white/50" />
         </PanelResizeHandle>
 
         {/* Right */}
         <Panel
           collapsible
-          defaultSize={20}
+          defaultSize={25}
           className="shadow-inner shadow-white/5 ring-offset-white/10 ring-offset-1 ring-1 ring-black/20 bg-neutral-900 rounded-lg"
         >
           <Properties />
--- src/components/Properties/index.tsx
+++ src/components/Properties/index.tsx
@@ -2,11 +2,11 @@ import { Leva } from "leva";
 
 export function Properties() {
   return (
-    <>
+    <div className="h-full flex flex-col">
       <h2 className="p-4 uppercase font-light text-xs tracking-widest text-gray-300 border-b border-white/10">
         Properties
       </h2>
-      <div className="p-2">
+      <div className="flex-1 overflow-y-auto p-2">
         <Leva
           neverHide
           fill
@@ -24,6 +24,6 @@ export function Properties() {
           }}
         />
       </div>
-    </>
+    </div>
   );
 }
